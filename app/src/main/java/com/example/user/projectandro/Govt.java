e4b2fea648a792e415d09e31f4546c9d94576fb1
Masuda Tuba <islamnourin420@gmail.com>
2018-09-29 04:36:48 +0600

    4th commit by Tuba

---
+++ app/src/main/java/com/example/user/projectandro/Govt.java
@@ -0,0 +1,13 @@
+package com.example.user.projectandro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Govt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_govt);
+    }
+}
